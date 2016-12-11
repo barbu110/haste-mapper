da796fa2f7527dda31eff8534dcb03591fd3e9ef
Victor Barbu <victorbarbu08@gmail.com>
2016-12-11 19:53:59 +0200

    Add the "files" configuration option and @ignoreModule comment interpreter
    Along with "root", the "files" can now be passed for those files to be
    separately scanned.
    
    Modules can now be marked as ignored (so they won't be added to the
    modules map) with the comment "@ignoreModule".

--- src/Module.js
+++ src/Module.js
@@ -3,21 +3,25 @@
  */
 
 const crypto = require('crypto');
-const docblock = require('./DependencyGraph/docblock');
+const Docblock = require('./DependencyGraph/docblock');
 const path = require('path');
+const fs = require('fs');
 
 type ModuleDataType = {
     srcPath: string,
     moduleName: string,
+    ignore: boolean,
 }
 
 class Module {
     _moduleName: string;
     _srcPath: string;
+    _ignore: boolean;
 
-    constructor({ srcPath, moduleName }: ModuleDataType) {
+    constructor({ srcPath, moduleName, ignore }: ModuleDataType) {
         this._moduleName = moduleName;
         this._srcPath = srcPath;
+        this._ignore = ignore;
     }
 
     moduleName(): string {
@@ -26,6 +30,10 @@ class Module {
 
     srcPath(): string {
         return this._srcPath;
+    }
+
+    isIgnored(): boolean {
+        return this._ignore;
     }
 }
 
--- src/scanModules.js
+++ src/scanModules.js
@@ -7,51 +7,144 @@ const fs = require('fs');
 const isNpmModule = require('./DependencyGraph/isNpmModule');
 const walk = require('fs-walk');
 const Docblock = require('./DependencyGraph/docblock');
+const Module = require('./Module');
+const waterfall = require('async').waterfall;
+const each = require('async').each;
 
 type ModuleScannerInitDataType = {
     root: string,
+    files: string[],
 };
 type ModulesList = { [moduleName: string]: string };
 
-function scanModules({ root }: ModuleScannerInitDataType): Promise<ModulesList> {
+/**
+ * Process a file from the given path.
+ *
+ * @param  {Docblock} parser The docblock parser.
+ * @param  {string} file The location of the file to parse
+ * @param  {(err: ?Object, m: ?Module, hasModule: bool)} callback
+ */
+function processFile(
+    parser: Docblock,
+    file: string,
+    callback: (err: ?Object, m: ?Module, hasModule: bool) => void
+): void {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        const docs = parser.extract(data);
+        const parsedDocs = parser.parseAsObject(docs);
+
+        if (parsedDocs.hasOwnProperty('providesModule')) {
+            const moduleName = parsedDocs.providesModule;
+            const ignore = parsedDocs.hasOwnProperty('ignoreModule');
+
+            const m = new Module({
+                srcPath: file,
+                moduleName,
+                ignore,
+            });
+
+            callback(undefined, m, true);
+        } else {
+            callback(undefined, undefined, false);
+        }
+    });
+}
+
+/**
+ * Scan modules with the given configuration.
+ */
+function scanModules({ root, files }: ModuleScannerInitDataType): Promise<ModulesList> {
     root = path.resolve(root);
     let modulesList: ModulesList = {};
+    const parser = new Docblock();
 
     const rootStat = fs.statSync(root);
     if (!rootStat.isDirectory()) {
-        throw new Error(`The provided path is invalid: ${root}`);
+        throw new Error(`The provided path is not a directory: ${root}`);
     }
 
     return new Promise((resolve, reject) => {
-        walk.files(root, (basedir, filename, stat, next) => {
-            const file = path.resolve(path.join(basedir, filename));
-            const parser = new Docblock();
+        waterfall([
+            (next) => walk.files(root, (basedir, filename, stat, nextFile) => {
+                const file = path.resolve(path.join(basedir, filename));
 
-            fs.readFile(file, 'utf8', (err, data) => {
-                if (err) {
-                    throw err;
-                }
+                processFile(parser, file, (err: ?Object, m: ?Module, hasModule: ?bool) => {
+                    if (err) {
+                        nextFile(err);
+                        return;
+                    }
+                    if (!hasModule) {
+                        nextFile();
+                        return;
+                    }
 
-                const docs = parser.extract(data);
-                const parsedData = parser.parseAsObject(docs);
-
-                if (parsedData.hasOwnProperty('providesModule')) {
-                    const moduleName = parsedData.providesModule;
+                    const moduleName = m.moduleName();
+                    const ignore = m.isIgnored();
+                    const moduleSource = m.srcPath();
 
                     if (modulesList.hasOwnProperty(moduleName)) {
-                        const original = modulesList[moduleName];
+                        const originalSource = modulesList[moduleName];
 
-                        return reject(new Error(`Duplicate definition of "${moduleName}" found in ${original} and ${moduleName}.`));
+                        nextFile(new Error(
+                            `Duplicate definition of "${moduleName}" found in ${original} and ${moduleName}.`
+                        ));
+                        return;
                     }
 
                     modulesList[moduleName] = file;
+                    nextFile();
+                });
+            }, (err) => {
+                if (err) {
+                    next(err);
+                    return;
                 }
-            });
+                next();
+            }),
+            (next) => {
+                if (!files) {
+                    next();
+                    return;
+                }
 
-            next();
-        }, (err) => {
+                each(files, (file: string, nextFile: Function) => {
+                    processFile(parser, file, (err: ?Object, m: ?Module, hasModule: ?bool) => {
+                        if (err) {
+                            nextFile(err);
+                            return;
+                        }
+                        if (!hasModule) {
+                            nextFile();
+                            return;
+                        }
+
+                        const moduleName = m.moduleName();
+                        const ignore = m.isIgnored();
+                        const moduleSource = m.srcPath();
+
+                        if (modulesList.hasOwnProperty(moduleName)) {
+                            const originalSource = modulesList[moduleName];
+
+                            nextFile(new Error(
+                                `Duplicate definition of "${moduleName}" found in ${original} and ${moduleName}.`
+                            ));
+                            return;
+                        }
+
+                        nextFile();
+                        modulesList[moduleName] = file;
+                    });
+                }, next);
+            },
+        ], (err) => {
             if (err) {
                 reject(err);
+                return;
             }
             resolve(modulesList);
         });
